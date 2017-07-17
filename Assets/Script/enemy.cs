c83944db10d8ca377c34392b85ca0d362c947a72
TAKU\Takumi <awatykhall850@gmail.com>
2017-07-17 22:36:07 +0900

    First

---
+++ Assets/Script/enemy.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemy : MonoBehaviour {
+
+	void Update (){
+		transform.Translate (new Vector3(-0.1f,1,0));
+		if (transform.position.z < Camera.main.transform.position.z) {
+			Destroy (gameObject);
+		}
+	}
+
+}
---
+++ Assets/Script/game.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class game : MonoBehaviour {
+
+	void Start () {
+		this.gameObject.GetComponent<Text>().enabled = false;
+	}
+
+	public void Lose () {
+		this.gameObject.GetComponent<Text>().enabled = true;
+	}
+}
